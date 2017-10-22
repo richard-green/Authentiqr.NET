f59220b9548ff842526d36271c3a230d7f77ad5a
Richard Green <rich@rdgreen.eu>
2017-10-22 15:40:29 +0100

    Hide window from Alt+Tab

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -108,6 +108,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(212, 76);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = global::Authentiqr.NET.Resources.Padlock;
             this.Location = new System.Drawing.Point(-1000, 0);
             this.Name = "frmMain";
